a6ccfbfd458f6f1ee9bcbcf318ff414184986529
Marc <marc.page@ni.com>
2018-03-30 11:44:14 -0500

    now we can store data and restore it

--- Compute.h
+++ Compute.h
@@ -6,10 +6,33 @@
 #include "os/ZCompression.h"
 #include "os/SymetricEncrypt.h"
 #include "protocol/JSON.h"
+#include "libernet/Storage.h"
 
 namespace compute {
 
+	std::string stash(const std::string &contents, store::Container &container, std::string &identifier, std::string &key) {
+		hash::sha256 	contentsHash(contents);
+		crypto::AES256	cryptor(contentsHash.data());
+		std::string		compressed= z::compress(contents, 9);
+		std::string		encrypted= cryptor.encrypt(compressed);
+		hash::sha256	finalHash(encrypted);
 
+		container.put(finalHash.hex(), encrypted);
+		return finalHash.hex(identifier) + ":" + contentsHash.hex(key);
+	}
+
+	std::string unstash(store::Container &store, const::std::string &identifier) {
+		std::string		encrypted= store.get(identifier.substr(0, identifier.find(':')));
+
+		printf("identifier: '%s'\n", identifier.c_str());
+		printf("contentsHash: %s\n", identifier.substr(identifier.find(':') + 1).c_str());
+		hash::sha256	contentsHash= hash::sha256::fromHex(identifier.substr(identifier.find(':') + 1));
+		crypto::AES256	cryptor(contentsHash.data());
+		std::string		compressed= cryptor.decrypt(encrypted);
+		std::string		contents= z::uncompress(compressed);
+
+		return contents;
+	}
 
 }
 
--- tests/Compute_test.cpp
+++ tests/Compute_test.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "libernet/Compute.h"
 #include "os/Thread.h"
 
@@ -5,18 +6,49 @@ int main(int argc, const char *argv[]) {
 #ifndef __Tracer_h__
 exec::ThreadId::sleep(1.05, exec::ThreadId::Seconds);
 #endif
-	for (int arg= 1; arg < argc; ++arg) {
-		try {
-			std::string 	contents= io::Path(argv[arg]).contents();
-			hash::sha256 	contentsHash(contents);
-			crypto::AES256	key(contentsHash.data());
-			std::string		compressed= z::compress(contents, 9);
-			std::string		encrypted= key.encrypt(compressed);
-			hash::sha256 	finalHash(encrypted);
-			printf("%s:%s\t%s\n", finalHash.hex().c_str(), contentsHash.hex().c_str(), argv[arg]);
-		} catch(const std::exception &exception) {
-			printf("EXCEPTION: %s\nUnable to process '%s'\n", exception.what(), argv[arg]);
+	try {
+		json::Value 			package;
+		std::string				contentsHash;
+		std::string				finalHash;
+		std::string 			contents;
+		std::string				identifier;
+		io::Path				location("bin/logs/compute");
+		io::Path				outDir("bin/logs/results");
+		store::Container		store(location);
+		json::Value::StringList	names;
+
+		package.makeObject();
+		for (int arg= 1; arg < argc; ++arg) {
+			try {
+				json::Value		fileInfo;
+
+				identifier= compute::stash(io::Path(argv[arg]).contents(), store, finalHash, contentsHash);
+				fileInfo.makeObject();
+				fileInfo["identifier"]= identifier;
+				fileInfo["sha256"]= finalHash;
+				fileInfo["aes256"]= contentsHash;
+				package[argv[arg]]= fileInfo;
+				printf("%s\t%s\n", argv[arg], identifier.c_str());
+			} catch(const std::exception &exception) {
+				printf("EXCEPTION: %s\nUnable to process '%s'\n", exception.what(), argv[arg]);
+			}
 		}
+
+		identifier= compute::stash(package, store, finalHash, contentsHash);
+		printf("%s\n", identifier.c_str());
+		package.parse(compute::unstash(store, identifier));
+		printf("RAW PACKAGE: %s\n", compute::unstash(store, identifier).c_str());
+		printf("PACKAGE: %s\n", std::string(package).c_str());
+		names= package.keys();
+		for (json::Value::StringList::iterator name= names.begin(); name != names.end(); ++name) {
+			io::Path	outFile= outDir + *name;
+
+			outFile.parent().mkdirs();
+			outFile.write(compute::unstash(store, package[*name]["identifier"].string()));
+		}
+		printf("%s\n", std::string(package).c_str());
+	} catch(const std::exception &exception) {
+		printf("EXCEPTION: %s\n", exception.what());
 	}
 	return 0;
 }
