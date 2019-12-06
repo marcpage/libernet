1e79a4d92345008b4dcf7a2d85f75a30bee7cca9
Marc Page <marcallenpage@gmail.com>
2019-12-06 11:21:34 -0600

    starting code to interpret a packaged directory

--- Package.h
+++ Package.h
@@ -13,6 +13,27 @@
 
 namespace pkg {
 
+	inline void splitIdentifier(const std::string &identifier, std::string &nameType, std::string &name, std::string &keyType, std::string &key) {
+		const std::string::size_type firstColon = identifier.find(':');
+		const std::string::size_type secondColon = identifier.find(':', firstColon + 1);
+		const std::string::size_type thirdColon = identifier.find(':', secondColon + 1);
+
+		nameType.clear();
+		name.clear();
+		keyType.clear();
+		key.clear();
+		if (std::string::npos != firstColon) {
+			nameType.assign(identifier, 0, firstColon);
+			if (std::string::npos != secondColon) {
+				name.assign(identifier, firstColon + 1, secondColon);
+				if (std::string::npos != secondColon) {
+					keyType.assign(identifier, secondColon + 1, thirdColon);
+					key.assign(identifier, thirdColon + 1);
+				}
+			}
+		}
+	}
+
 	inline std::string packData(const std::string &sourceData, std::string &packedData, std::string &dataName) {
 		hash::sha256		sourceHash(sourceData);
 		std::string			compressedData = z::compress(sourceData, 9);
@@ -82,6 +103,7 @@ namespace pkg {
 		for (auto entry = contents.begin(); entry != contents.end(); ++entry) {
 			if (!io::Path::endsWithPathSeparator(*entry)) {
 				listing["files"][io::Path(*entry).relativeTo(listingPathAbsolute)]["contents"] = encryptFile(*entry, storagePathAbsolute);
+				listing["files"][io::Path(*entry).relativeTo(listingPathAbsolute)]["size"] = io::Path(*entry).size();
 			} else {
 				json::Value directoryName;
 				directoryName = std::string(io::Path(*entry).relativeTo(listingPathAbsolute));
@@ -94,6 +116,33 @@ namespace pkg {
 
 	inline std::string packageDirectory(const io::Path &path, const io::Path &storagePath) {
 		return storeData(directoryInfo(path, storagePath), storagePath.absolute());
+	}
+
+	inline json::Value unpackageDirectoryInfo(const std::string &packagedDirectory, const std::string &identifier = std::string()) {
+		try {
+			return json::Value(packagedDirectory);
+		} catch(const json::WrongType &) {
+			std::string	nameType, name, keyType, key;
+
+			splitIdentifier(identifier, nameType, name, keyType, key);
+			if (nameType != "sha256") {
+				throw json::WrongType("Expected data identifier to be sha256, but found '" + nameType + "'", __FILE__, __LINE__);
+			} else if ( (keyType.size() > 0) && (keyType != "aes256/sha256") ) {
+				throw json::WrongType("Expected key type to be aes256/sha256 or blank, but found '" + keyType + "'", __FILE__, __LINE__);
+			} else if ( (keyType.size() == 0) && (key.size() != 0) ) {
+				throw json::WrongType("Expected empty key if keyType is empty, but found key: '" + key + "'", __FILE__, __LINE__);
+			} else if (key.size() == 0) {
+				throw json::WrongType("We have an empty key, but a key type of: '" + keyType + "'", __FILE__, __LINE__);
+			}
+			// verify hash of packagedDirectory matches sha256
+			// if key is empty, try to json parse the data and return it, else
+			// create key from key string
+			// decrypt data
+			// try to json parse data and return it, else
+			// decompress data, json parse, and return it
+
+		}
+
 	}
 
 
