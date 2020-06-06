ddae5ef760a8a5e23606e20cd8657704e2c3d64f
Marc Page <marcallenpage@gmail.com>
2020-06-05 20:09:42 -0500

    adding example apple code

--- src/os/AsymmetricEncrypt.h
+++ src/os/AsymmetricEncrypt.h
@@ -323,3 +323,125 @@ typedef OpenSSLRSAAES256PrivateKey RSAAES256PrivateKey;
 } // namespace crypto
 
 #endif // __AsymmetricEncrypt_h__
+
+/*
+// clang++ 2clean/AsymetricEncrypt.cpp -framework Security -framework CoreFoundation -DOpenSSLAvailable=1 -I.. -I2Clean -I/usr/local/Cellar/openssl@1.1/1.1.1d/include -L/usr/local/Cellar/openssl@1.1/1.1.1d/lib -lcrypto -o /tmp/test
+
+#include <stdio.h>
+#include <string>
+
+
+/*
+	Asymmetric Encrypt Behaviors
+		Public Key
+			1. Verify Signature
+			2. Serialize
+			3. Deserialize
+		Private Key
+			1. Sign
+			2. Serialize
+			3. Deserialize
+			4. Get public key
+			5. Generate
+•/
+
+
+// Recommended public exponents: 3, 5, 17, 257 or 65537
+
+#define AppleAPI 1
+
+#if AppleAPI
+
+#if __APPLE_CC__ || __APPLE__
+	#include <CommonCrypto/CommonCryptor.h>
+	#include <Security/Security.h>
+#endif
+
+std::string dumpKey(const void *key) {
+	OSStatus res;
+	CFDataRef exported;
+
+	if (!key) {
+		printf("key is null\n");
+		return "";
+	}
+	res = SecItemExport(key, kSecFormatUnknown, kSecItemPemArmour, nullptr, &exported);
+	if (errSecPassphraseRequired == res) {
+		printf("Passphrase Required\n");
+		return "";
+	} else if (!exported) {
+		printf("Unable to export key data\n");
+		return "";
+	}
+	CFIndex size = CFDataGetLength(exported);
+
+	std::string value(std::string::size_type(size), '\0');
+	CFDataGetBytes(exported, CFRangeMake(0,size), reinterpret_cast<UInt8*>(const_cast<char*>(value.data())));
+	printf("value = '%s'\n", value.c_str());
+	return value;
+}
+
+int main(int argc, char* argv[]) {
+	SecKeyRef publicKey, privateKey;
+	OSStatus res;
+	int keySize = (argc == 2) ? atoi(argv[1]) : 1024; // 1024 - 4096 increment 8
+
+	/* Time to generate key
+		1024 = 0.25 seconds
+		2048 - 2 seconds
+		3072 - 8 seconds
+		4096 - 30 seconds
+	•/
+
+    SecItemImportExportKeyParameters params;
+    SecExternalItemType itemType = kSecItemTypePublicKey;
+    SecExternalFormat format = kSecFormatOpenSSL;
+
+	memset(&params, 0, sizeof(params));
+	params.flags = kSecKeyNoAccessControl;
+	params.passphrase = nullptr;
+
+	CFMutableDictionaryRef attrDict = CFDictionaryCreateMutable(nullptr, 3, &kCFTypeDictionaryKeyCallBacks, nullptr);
+	CFDictionaryAddValue(attrDict, kSecAttrKeyType, kSecAttrKeyTypeRSA);
+	CFDictionaryAddValue(attrDict, kSecAttrKeySizeInBits, CFNumberCreate(nullptr, kCFNumberIntType, &keySize));
+
+	res = SecKeyGeneratePair(attrDict, &publicKey, &privateKey);
+
+	const void *elements[] = {publicKey, privateKey};
+	CFArrayRef keys = CFArrayCreate(nullptr, elements, 2, &kCFTypeArrayCallBacks);
+	std::string keyText = dumpKey(keys);
+	std::string pub = dumpKey(publicKey);
+	std::string priv = dumpKey(privateKey);
+
+	CFArrayRef keyList = nullptr;
+	CFDataRef data;
+
+	data = CFDataCreate(nullptr, reinterpret_cast<const UInt8 *>(pub.data()), pub.size());
+	itemType = kSecItemTypePublicKey;
+	res = SecItemImport(data, nullptr, &format, &itemType, 0, &params, nullptr, &keyList); // crash
+	printf("res=%d\n", res);
+	printf("length=%ld\n", (long)CFArrayGetCount(keyList));
+
+	SecKeyRef pubCopy = reinterpret_cast<SecKeyRef>(const_cast<void*>(CFArrayGetValueAtIndex(keyList, 0)));
+	printf("pubCopy = %p\n", pubCopy);
+	dumpKey(pubCopy);
+
+	data = CFDataCreate(nullptr, reinterpret_cast<const UInt8 *>(priv.data()), priv.size());
+	itemType = kSecItemTypePrivateKey;
+	res = SecItemImport(data, nullptr, &format, &itemType, 0, &params, nullptr, &keyList);
+	printf("res=%d\n", res);
+	printf("length=%ld\n", (long)CFArrayGetCount(keyList));
+	printf("Keysize = %d\n", keySize);
+	// SecKeyCreateEncryptedData https://developer.apple.com/documentation/security/1643957-seckeycreateencrypteddata?language=objc
+
+	SecKeyRef privCopy = reinterpret_cast<SecKeyRef>(const_cast<void*>(CFArrayGetValueAtIndex(keyList, 0)));
+	if (CFGetTypeID(privCopy) != SecKeyGetTypeID()) {
+		printf("Not a SecKey\n");
+	}
+	printf("privCopy = %p\n", privCopy);
+	dumpKey(privCopy);
+
+	return 0;
+}
+*/
+
