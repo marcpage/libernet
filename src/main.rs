edefeea382a2751b395d88333432115c868eeece
Marc Page <Marc.Page@ni.com>
2021-09-02 21:18:10 -0500

    Starting to implement the api

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use data_encoding::HEXLOWER;
 use ring::digest::{Context, Digest, SHA256};
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
+use warp::{Filter, http::Response};
 
 error_chain! {
     foreign_links {
@@ -27,6 +28,85 @@ fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
     Ok(context.finish())
 }
 
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let matches = App::new("libernet")
+                    .version("0.0.1")
+                    .author("Marc Page <MarcAllenPage@gmail.com")
+                    .about("Wiki for the world")
+                    .arg(Arg::with_name("port")
+                        .short("p")
+                        .long("port")
+                        .value_name("PORT")
+                        .help("The port to listen on")).get_matches();
+    // implement /web, /{app}, and PUT for /server, /data/sha256/{hash}
+    let rest = warp::path::end().map(|| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body("<html><body><b>Index App</b></body></html>")
+        }).or(warp::path("api")
+            .and(warp::path::param())
+            .and(warp::path::end())
+            .map(|action: String| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>Action: {}</b></body></html>", action))
+        })).or(warp::path("app")
+            .and(warp::path::end())
+            .map(|| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>app Configure</b></body></html>"))
+        })).or(warp::path("server")
+            .and(warp::path::end())
+            .map(|| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>server info</b></body></html>"))
+        })).or(warp::path("data")
+            .and(warp::path("requests"))
+            .and(warp::path::end())
+            .map(|| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>data requests</b></body></html>"))
+        })).or(warp::path("data")
+            .and(warp::path("sha256"))
+            .and(warp::path::param())
+            .and(warp::path::end())
+            .map(|hash: String| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>data: hash={}</b></body></html>", hash))
+        })).or(warp::path("data")
+            .and(warp::path("like"))
+            .and(warp::path("sha256"))
+            .and(warp::path::param())
+            .and(warp::path::end())
+            .map(|hash: String| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>data like: hash={}</b></body></html>", hash))
+        })).or(warp::path("data")
+            .and(warp::path("sha256"))
+            .and(warp::path::param())
+            .and(warp::path("aes256"))
+            .and(warp::path::param())
+            .and(warp::path::end())
+            .map(|hash: String, key: String| {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>data: hash={} key={}</b></body></html>", hash, key))
+        }));
+
+    let port:u16 = matches.value_of("port").unwrap_or("80").parse::<u16>().unwrap_or(80);
+    warp::serve(rest)
+        .run(([127, 0, 0, 1], port))
+        .await;
+}
+
+/*
 fn main() -> Result<()> {
     let matches = App::new("libernet")
                     .version("0.0.1")
@@ -54,3 +134,4 @@ fn main() -> Result<()> {
 
     Ok(())
 }
+*/
