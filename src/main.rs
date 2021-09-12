fa66a6d46c5365b68f4d19dee04be442a39b467f
Marc Page <Marc.Page@ni.com>
2021-09-12 13:55:55 -0500

    Serving on all interfaces, protecting access to private (web user) locations, adding PUT locations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use warp::Filter;
 
+fn is_local_link(addr: std::net::IpAddr) -> bool {
+    let mut is_local = false;
+
+    for iface in pnet::datalink::interfaces() {
+        for address in iface.ips {
+            if address.ip() == addr {
+                is_local = true
+            }
+        }
+    }
+    is_local
+}
+
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
@@ -12,38 +25,88 @@ async fn main() {
                         .long("port")
                         .value_name("PORT")
                         .help("The port to listen on")).get_matches();
-    // implement /web, /{app}, and PUT for /server, /data/sha256/{hash}
-    let rest = warp::path::end().map(|| {
-        warp::http::Response::builder()
-            .header("Content-Type", "text/html")
-            .body("<html><body><b>Index App</b></body></html>")
-        }).or(warp::path("api")
+
+    // Private GET root (/)
+    let rest = warp::get()
+            .and(warp::addr::remote())
+            .and(warp::path::end()).map(|addr: Option<std::net::SocketAddr>| {
+        if !is_local_link(addr.unwrap().ip()) {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .status(401)
+                .body(format!("<html><body><b>Index App</b><br/><span style='color:red'>Not allowed</span><br/>From: {:?}</body></html>",addr))
+        } else {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .status(401)
+                .body(format!("<html><body><b>Index App</b><br/>From: {:?}</body></html>",addr))
+        }
+
+        // Private GET /api/<action>
+        }).or(warp::get()
+            .and(warp::addr::remote())
+            .and(warp::path("api"))
             .and(warp::path::param())
             .and(warp::path::end())
-            .map(|action: String| {
-        warp::http::Response::builder()
-            .header("Content-Type", "text/html")
-            .body(format!("<html><body><b>Action: {}</b></body></html>", action))
-        })).or(warp::path("app")
+            .map(|addr: Option<std::net::SocketAddr>, action: String| {
+        if !is_local_link(addr.unwrap().ip()) {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .status(401)
+                .body(format!("<html><body><b>Index App</b><br/><span style='color:red'>Not allowed</span><br/>From: {:?}</body></html>",addr))
+        } else {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>Action: {}</b> @ {:?}</body></html>", action, addr))
+        }
+
+        // Private GET /app
+        })).or(warp::get()
+            .and(warp::addr::remote())
+            .and(warp::path("app"))
             .and(warp::path::end())
-            .map(|| {
-        warp::http::Response::builder()
-            .header("Content-Type", "text/html")
-            .body(format!("<html><body><b>app Configure</b></body></html>"))
-        })).or(warp::path("server")
+            .map(|addr: Option<std::net::SocketAddr>| {
+        if !is_local_link(addr.unwrap().ip()) {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>app Configure</b> ({:?})<span style='color:red'>Not allowed</span></body></html>", addr))
+        } else {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>app Configure</b> ({:?})</body></html>", addr))
+        }
+
+        // Public GET /server
+        })).or(warp::get()
+            .and(warp::path("server"))
             .and(warp::path::end())
             .map(|| {
         warp::http::Response::builder()
             .header("Content-Type", "text/html")
             .body(format!("<html><body><b>server info</b></body></html>"))
-        })).or(warp::path("data")
+
+        // Public PUT /server
+        })).or(warp::put()
+            .and(warp::path("server"))
+            .and(warp::path::end())
+            .map(|| {
+        warp::http::Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>server info</b></body></html>"))
+
+        // Public GET /data/requests
+        })).or(warp::get()
+            .and(warp::path("data"))
             .and(warp::path("requests"))
             .and(warp::path::end())
             .map(|| {
         warp::http::Response::builder()
             .header("Content-Type", "text/html")
             .body(format!("<html><body><b>data requests</b></body></html>"))
-        })).or(warp::path("data")
+
+        // Public GET /data/sha256/<hash>
+        })).or(warp::get()
+            .and(warp::path("data"))
             .and(warp::path("sha256"))
             .and(warp::path::param())
             .and(warp::path::end())
@@ -51,7 +114,21 @@ async fn main() {
         warp::http::Response::builder()
             .header("Content-Type", "text/html")
             .body(format!("<html><body><b>data: hash={}</b></body></html>", hash))
-        })).or(warp::path("data")
+
+        // Public PUT /data/sha256/<hash>
+        })).or(warp::put()
+            .and(warp::path("data"))
+            .and(warp::path("sha256"))
+            .and(warp::path::param())
+            .and(warp::path::end())
+            .map(|hash: String| {
+        warp::http::Response::builder()
+            .header("Content-Type", "text/html")
+            .body(format!("<html><body><b>data: hash={}</b></body></html>", hash))
+
+        // Public GET /data/like/sha256/<hash>
+        })).or(warp::get()
+            .and(warp::path("data"))
             .and(warp::path("like"))
             .and(warp::path("sha256"))
             .and(warp::path::param())
@@ -60,7 +137,10 @@ async fn main() {
         warp::http::Response::builder()
             .header("Content-Type", "text/html")
             .body(format!("<html><body><b>data like: hash={}</b></body></html>", hash))
-        })).or(warp::path("data")
+
+        // Public GET /data/sha256/<hash>/aes256/<key>
+        })).or(warp::get()
+            .and(warp::path("data"))
             .and(warp::path("sha256"))
             .and(warp::path::param())
             .and(warp::path("aes256"))
@@ -70,11 +150,44 @@ async fn main() {
         warp::http::Response::builder()
             .header("Content-Type", "text/html")
             .body(format!("<html><body><b>data: hash={} key={}</b></body></html>", hash, key))
+
+        // Private GET /web/<path>
+        })).or(warp::get()
+            .and(warp::addr::remote())
+            .and(warp::path("web"))
+            .and(warp::path::tail())
+            .map(|addr: Option<std::net::SocketAddr>, path: warp::path::Tail| {
+        if !is_local_link(addr.unwrap().ip()) {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>web</b>: {}<br/><span style='color:red'>Not allowed</span></body></html>", path.as_str()))
+        } else {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>web</b>: {}</body></html>", path.as_str()))
+        }
+
+        // Private GET /<app>/<app path>
+        })).or(warp::get()
+            .and(warp::addr::remote())
+            .and(warp::path::param())
+            .and(warp::path::tail())
+            .map(|addr: Option<std::net::SocketAddr>, app_name: String, path: warp::path::Tail| {
+        if !is_local_link(addr.unwrap().ip()) {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>{}</b>: {}<br/><span style='color:red'>Not allowed</span></body></html>", app_name, path.as_str()))
+        } else {
+            warp::http::Response::builder()
+                .header("Content-Type", "text/html")
+                .body(format!("<html><body><b>{}</b>: {}</body></html>", app_name, path.as_str()))
+        }
+
         }));
 
     let port:u16 = matches.value_of("port").unwrap_or("8000").parse::<u16>().unwrap_or(80);
     warp::serve(rest)
-        .run(([127, 0, 0, 1], port))
+        .run(([0, 0, 0, 0], port))
         .await;
 }
 
