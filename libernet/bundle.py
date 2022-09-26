2d731a7d42b8a21752e3389fb635603368564469
Marc Page <MarcAllenPage@gmail.com>
2022-09-26 13:08:13 -0500

    Documenting the untested code in libernet.bundle

--- libernet/bundle.py
+++ libernet/bundle.py
@@ -60,13 +60,13 @@ def handle_args(args):
         return results[0]
 
     if args.action == "restore":
-        if not args.url:
+        if not args.url:  # not tested
             print("For restore you must specify a --url/-u")
             sys.exit(1)
 
         missing_blocks = libernet.tools.bundle.missing_blocks(args.url, args.storage)
 
-        if missing_blocks:
+        if missing_blocks:  # not tested
             print("The following blocks are missing and need to be loaded first")
             print("\t" + "\n\t".join(missing_blocks))
             sys.exit(1)
@@ -74,7 +74,7 @@ def handle_args(args):
         bundle = libernet.tools.bundle.Path(args.url, args.storage)
         missing = bundle.missing_blocks()
 
-        if missing:
+        if missing:  # not tested
             print("Missing blocks:")
             print("\t" + "\n\t".join(missing))
             sys.exit(1)
@@ -82,14 +82,15 @@ def handle_args(args):
         bundle.restore_file(args.dir)
         return True
 
+    # not tested
     print("Unknown action: " + args.action)
     sys.exit(1)
 
 
-def main():
+def main():  # not tested
     """Entry point. Loop forever unless we are told not to."""
     handle_args(get_arg_parser().parse_args())
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # not tested
     main()
